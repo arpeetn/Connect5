c9683f74e2ff5ebdfdedac1b9a8c748f656c2f03
Arpeet Naik <99459904+arpeetn@users.noreply.github.com>
2022-03-30 10:53:48 -0400

    Add files via upload

---
+++ AbsGameBoard.java
@@ -0,0 +1,45 @@
+package cpsc2150.extendedConnectX.models;
+
+public abstract class AbsGameBoard implements IGameBoard {
+
+    /**
+     *This method creates a string to show row and column position
+     *
+     * @return one string that shows the entire game board
+     *
+     * @pre none
+     * @post string that shows the entire game board = {"
+    |0|1|2|3|4|5|6|7|8|
+    | | | | | | | | | |
+    | | | | | | | | | |
+    | | | | | | | | | |
+    | | | | | | | | | |
+    | | | | | | | | | |
+    | | | | | | | | | |
+    "}
+     * [all of the empty spaces in the game board above will be filled with either an empty space, X, or O
+     *
+     * if gameboard[i][j] == ' ', the empty space will stay empty
+     * if gameboard[i][j] == 'X', the empty space will have an X inside
+     * if gameboard[i][j] == 'O', the empty space will have a O inside]
+     *
+     */
+
+    @Override
+    public String toString() {
+        String board = "|";
+        for (int i = 0; i < getNumColumns(); i++) board += i + "|";
+
+        board += "\n|";
+
+        for (int i = getNumRows()-1; i >= 0; i--) {
+            for(int j = 0; j < getNumColumns(); j++) {
+                BoardPosition pos = new BoardPosition(i,j);
+                board += whatsAtPos(pos) + "|";
+            }
+            if (i - 1 != -1) board += "\n|";
+        }
+        board+="\n";
+        return board;
+    }
+}
